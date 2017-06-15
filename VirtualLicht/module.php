194a03929feb19ef4e5fe01cbb9bebffdb2d3f18
Hagbard235 <github@andreschmidt.de>
2017-06-15 12:35:16 +0200

    .

--- VirtualLicht/module.php
+++ VirtualLicht/module.php
@@ -57,8 +57,7 @@
         // Überschreibt die intere IPS_ApplyChanges($id) Funktion
         public function ApplyChanges() {
             // Diese Zeile nicht löschen
-            IPS_SetProperty($this->InstanceID, "ToggleScriptID", 99);
-			
+            
 			parent::ApplyChanges();
 			//Instanz ist aktiv
 			$this->SetStatus(102);
@@ -74,11 +73,408 @@
         * ABC_MeineErsteEigeneFunktion($id);
         *
         */
-        public function LichtSchalten() {
-            //Eigener Coder
+        public function LichtUmschalten() {
+            if (($_IPS['SENDER'] == 'WebFront') or ($_IPS['SENDER'] == "AlexaSmartHome"))
+		{
+		if ($_IPS['VALUE'] === true)
+			{
+			LichtAnschalten();
+			}
+			
+		  else
+			{
+			LichtAusschalten();
+			}
+		}
+	  else
+		{
+		if ($_IPS['SENDER'] == "TimerEvent")
+			{
+
+			// Timer ausschalten
+
+			$this->SetTimerInterval("Umschalten", 0);
+			// Vergleichen und ggf. Fehlermeldung oder nochmal versuchen
+
+			$statusvar = GetIDForIdent("Status");
+			if (IPS_VariableExists($statusvar)) {
+				
+			
+
+				}
+				else {
+				$VarID_NEU = $this->RegisterVariableBoolean("Status","Status");
+				
+				
+				}
+			$O_ID = $this->ReadPropertyString("PropertyInstanceID");
+			$hw_statusvar = @IPS_GetObjectIDByName("Status", $O_ID);
+			if ($hw_statusvar === false)
+				{
+			//	sendDBMessage('ERROR', 'In ' . $_IPS['SELF'] . ' wurde die Statusvariable_Hardware nicht gefunden', '', 0);
+			//	return false;
+				}
+
+			if (GetValueBoolean($hw_statusvar) != GetValueBoolean($statusvar))
+				{
+
+				// Differenz entedeckt!
+////TODO anderen Variablen-Typ wählen
+
+				$CountVar = GetIDForIdent("Count");
+				
+				if ($CountVar === false)
+					{
+
+					// 1. Versuch
+
+				//	sendDBMessage('ERROR', 'V1-In ' . $_IPS['SELF'] . ' wurde nicht richtig geschaltet', '', 0);
+								IPS_LogMessage($_IPS['SELF'], "DEBUG:Schalten im 1. Versuch nicht erfolgreich");
+
+					$VarID_Count = $this->RegisterVariableBoolean("Count","Count");
+					
+					SetValueBoolean($VarID_Count, false);
+
+					// 2. Versuch starten
+
+					$Scriptname = "Toggle";
+					$O_ID = $this->ReadPropertyString("PropertyInstanceID");
+					$scriptAn = @IPS_GetObjectIDByName($Scriptname, $O_ID);
+					$statusvar = GetIDForIdent("Status");
+					if ($statusvar === false)
+						{
+				//		sendDBMessage('ERROR', 'In ' . $_IPS['SELF'] . ' wurde die Statusvariable nicht gefunden', '', 0);
+				//		return false;
+						}
+
+					if (IPS_ScriptExists($scriptAn))
+						{
+						@IPS_RunScript($scriptAn);
+						SetValueBoolean($statusvar, !GetValueBoolean($statusvar));
+						}
+					  else
+						{
+				//		sendDBMessage('ERROR', 'In ' . $_IPS['SELF'] . ' wurde das passende Script nicht gefunden', '', 0);
+						}
+
+					// Neuen Timer aufziehen für 2. Versuch
+					$this->SetTimerInterval("Umschalten", 20000);
+					}
+				  else
+					{
+
+					// 2. Durchlauf und immer noch nich geschaltet
+
+				//	sendDBMessage('ERROR', 'V2-In ' . $_IPS['SELF'] . ' wurde nicht richtig geschaltet nach Wiederholung', '', 0);
+
+					// IPS_DeleteVariable($CountVar);
+					// Keinen neuen Timer, hier Ende
+
+					}
+				}
+			  else
+				{
+
+				// Keine Differenz (mehr) entdeckt, mögliche Variable löschen
+
+				$CountVar = GetIDForIdent("Count");
+				if ($CountVar === false)
+					{
+
+					// war gar nicht da, also nie Differenz gehabt
+
+					}
+				  else
+					{
+					UnregisterVariable("Count");
+					}
+				}
+			}
+		  else
+			{
+			$Scriptname = "Toggle";
+			$O_ID = $this->ReadPropertyString("PropertyInstanceID");
+			$scriptAn = @IPS_GetObjectIDByName($Scriptname, $O_ID);
+			$statusvar = GetIDForIdent("Status");
+			if ($statusvar === false)
+				{
+		//		sendDBMessage('ERROR', 'In ' . $_IPS['SELF'] . ' wurde die Statusvariable nicht gefunden', '', 0);
+		//		return false;
+				}
+
+			if (IPS_ScriptExists($scriptAn))
+				{
+				@IPS_RunScript($scriptAn);
+				SetValueBoolean($statusvar, !GetValueBoolean($statusvar));
+				}
+			  else
+				{
+		//		sendDBMessage('ERROR', 'In ' . $_IPS['SELF'] . ' wurde das passende Script nicht gefunden', '', 0);
+				}
+
+			// Timer anschalten
+
+			$this->SetTimerInterval("Umschalten", 14000);
+	//		return false;
+			}
+		}
+	}
 				
 
         }
+		 public function LichtAnschalten() {
+			 if ($_IPS['SENDER'] == "TimerEvent")
+		{
+
+		// Timer ausschalten
+
+		$this->SetTimerInterval("Anschalten", 0);
 		
-    }
+
+		// Vergleichen und ggf. Fehlermeldung oder nochmal versuchen
+
+		$statusvar = GetIDForIdent("Status");
+		if ($statusvar === false)
+			{
+		//	sendDBMessage('ERROR', 'In ' . $_IPS['SELF'] . ' wurde die Statusvariable nicht gefunden', '', 0);
+		//	return false;
+			}
+
+		$O_ID =  $this->ReadPropertyString("PropertyInstanceID");
+		$hw_statusvar = @IPS_GetObjectIDByName("Status", $O_ID);
+		if ($hw_statusvar === false)
+			{
+		//	sendDBMessage('ERROR', 'In ' . $_IPS['SELF'] . ' wurde die Statusvariable_Hardware nicht gefunden', '', 0);
+		//	return false;
+			}
+
+		if (GetValueBoolean($hw_statusvar) != GetValueBoolean($statusvar))
+			{
+
+			// Differenz entedeckt!
+
+			$CountVar = GetIDForIdent("Count");
+			if ($CountVar === false)
+				{
+
+				// 1. Versuch
+
+				//sendDBMessage('ERROR', 'V1-In ' . $_IPS['SELF'] . ' wurde nicht richtig geschaltet', '', 0);
+				IPS_LogMessage($_IPS['SELF'], "DEBUG:Schalten im 1. Versuch nicht erfolgreich");
+				$VarID_Count =  $this->RegisterVariableBoolean("Count","Count");
+				SetValueBoolean($VarID_Count, false);
+
+				// 2. Schaltversuch
+
+				$Scriptname = "An_Force";
+				$O_ID =  $this->ReadPropertyString("PropertyInstanceID");
+				$scriptAn = @IPS_GetObjectIDByName($Scriptname, $O_ID);
+				$statusvar = GetIDForIdent("Status");
+				if ($statusvar === false)
+					{
+				//	sendDBMessage('ERROR', 'In ' . $_IPS['SELF'] . ' wurde die Statusvariable nicht gefunden', '', 0);
+				//	return false;
+					}
+
+				if (IPS_ScriptExists($scriptAn))
+					{
+					@IPS_RunScript($scriptAn);
+					SetValueBoolean($statusvar, true);
+					}
+				  else
+					{
+				//	sendDBMessage('ERROR', 'In ' . $_IPS['SELF'] . ' wurde das passende Script nicht gefunden', '', 0);
+					}
+
+				// Neuen Timer aufziehen für 2. Versuch
+
+				$this->SetTimerInterval("Anschalten", 20000);
+				}
+			  else
+				{
+
+				// 2. Durchlauf und immer noch nich geschaltet
+
+		//		sendDBMessage('ERROR', 'V2-In ' . $_IPS['SELF'] . ' wurde nicht richtig geschaltet nach Wiederholung', '', 0);
+
+				// IPS_DeleteVariable($CountVar);
+				// Keinen neuen Timer, hier Ende
+
+				}
+			}
+		  else
+			{
+
+			// Keine Differenz (mehr) entdeckt, mögliche Variable löschen
+
+			$CountVar = GetIDForIdent("Count");
+			if ($CountVar === false)
+				{
+
+				// war gar nicht da, also nie Differenz gehabt
+
+				}
+			  else
+				{
+				UnregisterVariable("Count");
+				}
+			}
+		}
+	  else
+		{
+		$Scriptname = "An";
+		$O_ID =  $this->ReadPropertyString("PropertyInstanceID");
+		$scriptAn = @IPS_GetObjectIDByName($Scriptname, $O_ID);
+		$statusvar = GetIDForIdent("Status");
+		if ($statusvar === false)
+			{
+		//	sendDBMessage('ERROR', 'In ' . $_IPS['SELF'] . ' wurde die Statusvariable nicht gefunden', '', 0);
+		//	return false;
+			}
+
+		if (IPS_ScriptExists($scriptAn))
+			{
+			@IPS_RunScript($scriptAn);
+			SetValueBoolean($statusvar, true);
+			}
+		  else
+			{
+	//		sendDBMessage('ERROR', 'In ' . $_IPS['SELF'] . ' wurde das passende Script nicht gefunden', '', 0);
+			}
+
+		// Timer anschalten
+
+		$this->SetTimerInterval("Anschalten", 14000);
+		//return false;
+		}
+                   }
+				   
+				   
+	 function LichtAusschalten()	{ 
+	 if ($_IPS['SENDER'] == "TimerEvent")
+		{
+
+		// Timer ausschalten
+
+		$this->SetTimerInterval("Ausschalten", 0);
+		
+
+		// Vergleichen und ggf. Fehlermeldung oder nochmal versuchen
+
+		$statusvar = GetIDForIdent("Status");
+		if ($statusvar === false)
+			{
+		//	sendDBMessage('ERROR', 'In ' . $_IPS['SELF'] . ' wurde die Statusvariable nicht gefunden', '', 0);
+		//	return false;
+			}
+
+		$O_ID =  $this->ReadPropertyString("PropertyInstanceID");
+		$hw_statusvar = @IPS_GetObjectIDByName("Status", $O_ID);
+		if ($hw_statusvar === false)
+			{
+		//	sendDBMessage('ERROR', 'In ' . $_IPS['SELF'] . ' wurde die Statusvariable_Hardware nicht gefunden', '', 0);
+		//	return false;
+			}
+
+		if (GetValueBoolean($hw_statusvar) != GetValueBoolean($statusvar))
+			{
+
+			// Differenz entedeckt!
+
+			$CountVar = GetIDForIdent("Count");
+			if ($CountVar === false)
+				{
+
+				// 1. Versuch
+
+				//sendDBMessage('ERROR', 'V1-In ' . $_IPS['SELF'] . ' wurde nicht richtig geschaltet', '', 0);
+				IPS_LogMessage($_IPS['SELF'], "DEBUG:Schalten im 1. Versuch nicht erfolgreich");
+				$VarID_Count =  $this->RegisterVariableBoolean("Count","Count");
+				SetValueBoolean($VarID_Count, false);
+
+				// 2. Schaltversuch
+
+				$Scriptname = "Aus_Force";
+				$O_ID =  $this->ReadPropertyString("PropertyInstanceID");
+				$scriptAn = @IPS_GetObjectIDByName($Scriptname, $O_ID);
+				$statusvar = GetIDForIdent("Status");
+				if ($statusvar === false)
+					{
+				//	sendDBMessage('ERROR', 'In ' . $_IPS['SELF'] . ' wurde die Statusvariable nicht gefunden', '', 0);
+				//	return false;
+					}
+
+				if (IPS_ScriptExists($scriptAn))
+					{
+					@IPS_RunScript($scriptAn);
+					SetValueBoolean($statusvar, true);
+					}
+				  else
+					{
+				//	sendDBMessage('ERROR', 'In ' . $_IPS['SELF'] . ' wurde das passende Script nicht gefunden', '', 0);
+					}
+
+				// Neuen Timer aufziehen für 2. Versuch
+
+				$this->SetTimerInterval("Ausschalten", 20000);
+				}
+			  else
+				{
+
+				// 2. Durchlauf und immer noch nich geschaltet
+
+		//		sendDBMessage('ERROR', 'V2-In ' . $_IPS['SELF'] . ' wurde nicht richtig geschaltet nach Wiederholung', '', 0);
+
+				// IPS_DeleteVariable($CountVar);
+				// Keinen neuen Timer, hier Ende
+
+				}
+			}
+		  else
+			{
+
+			// Keine Differenz (mehr) entdeckt, mögliche Variable löschen
+
+			$CountVar = GetIDForIdent("Count");
+			if ($CountVar === false)
+				{
+
+				// war gar nicht da, also nie Differenz gehabt
+
+				}
+			  else
+				{
+				UnregisterVariable("Count");
+				}
+			}
+		}
+	  else
+		{
+		$Scriptname = "Aus";
+		$O_ID =  $this->ReadPropertyString("PropertyInstanceID");
+		$scriptAn = @IPS_GetObjectIDByName($Scriptname, $O_ID);
+		$statusvar = GetIDForIdent("Status");
+		if ($statusvar === false)
+			{
+		//	sendDBMessage('ERROR', 'In ' . $_IPS['SELF'] . ' wurde die Statusvariable nicht gefunden', '', 0);
+		//	return false;
+			}
+
+		if (IPS_ScriptExists($scriptAn))
+			{
+			@IPS_RunScript($scriptAn);
+			SetValueBoolean($statusvar, true);
+			}
+		  else
+			{
+	//		sendDBMessage('ERROR', 'In ' . $_IPS['SELF'] . ' wurde das passende Script nicht gefunden', '', 0);
+			}
+
+		// Timer anschalten
+
+		$this->SetTimerInterval("Ausschalten", 14000);
+		//return false;
+		}	} // IPS_LichtAus
+
+    
 ?>
